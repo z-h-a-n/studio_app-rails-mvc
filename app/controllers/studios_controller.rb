7666163518729d7743ada6de5deb5fe7834cffd7
zhan wang <zhanzw@gmail.com>
2015-06-03 09:28:28 +0100

    working no css

--- app/controllers/studios_controller.rb
+++ app/controllers/studios_controller.rb
@@ -7,6 +7,31 @@ class StudiosController < ApplicationController
     @studio = Studio.find(params[:id])
   end
 
+  def new
+    @studio = Studio.new
+  end
+
+  def create
+    Studio.create(studio_params)
+    redirect_to(studios_path)
+  end
+
+  def edit
+    @studio = Studio.find(params[:id])
+  end
+
+  def update
+    @studio = Studio.find(params[:id])
+    @studio.update(studio_params)
+    redirect_to(studios_path)
+  end
+
+  def destroy
+    studio = Studio.find(params[:id])
+    studio.destroy
+    redirect_to(studios_path)
+  end
+
   private
   def studio_params
     params.require(:studio).permit(:name, :location, :website, :work_sample)
